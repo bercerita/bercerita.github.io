bf871e061b4aec8b43d8ac21c04ff5dd22c7b709
mzaini30 <muhzaini30@gmail.com>
2020-04-10 21:28:59 +0800

    upload

--- pug/index.js
+++ pug/index.js
@@ -1,7 +1,27 @@
+var olahData = () => {
+
+}
+
 if (!localStorage.id){
 	$('.modalLogin').modal()
+} else {
+	olahData()
 }
 
 $('.formLogin').on('submit', x => {
 	x.preventDefault()
+	loadingStart()
+	$.get(database, data => {
+		loadingDone()
+		var datanya = new OlahJson(data)
+		var cek = datanya.query(`user?username=${$('.loginUsername').val()}&password=${$('.loginPassword').val()}`).get()
+		if (cek.length > 0){
+			localStorage.setItem('id', cek[0].id)
+			localStorage.setItem('username', $('.loginUsername').val())
+			$('.modalLogin').modal('hide')
+			olahData()
+		} else {
+			alert('Username dan password salah')
+		}
+	})
 })
--- pug/template.js
+++ pug/template.js
@@ -1,1 +1,4 @@
 var database = 'https://jsonblob.com/api/3a003d6e-7b22-11ea-a2ae-dfc2463fdc04'
+
+var loadingStart = () => $('.loading').removeClass('sembunyi')
+var loadingDone = () => $('.loading').addClass('sembunyi')
